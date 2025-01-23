7e7df650baa92f0223d62609a7e8c015434f20b8
Abhishek Dasgupta <abhishek.dasgupta@dtc.ox.ac.uk>
2025-01-23 16:11:11 +0000

    update

--- memoryraster.py
+++ memoryraster.py
@@ -13,22 +13,32 @@ use as R's terra package.
 """
 
 from __future__ import annotations
+
+import xarray as xr
 import copy
 import dataclasses
 from pathlib import Path
+from contextlib import contextmanager
 
 import affine
-import rasterio
+import exactextract
 import geopandas as gpd
 import numpy as np
-import rasterio.mask
+import pandas as pd
+import rasterio
 import rasterio.enums
+import rasterio.mask
 import rasterio.plot
+
+from rasterio.transform import from_origin
 from rasterio.io import MemoryFile
 from rasterio.warp import calculate_default_transform, reproject
 
-
 DEFAULT_COLORMAP = "GnBu"
+
+
+def grid_size(da: xr.DataArray, axis: str) -> float:
+    return abs(da[axis][1] - da[axis][0])
 
 
 def get_numpy_dtype(t: str):
@@ -47,12 +57,13 @@ def get_numpy_dtype(t: str):
 
 @dataclasses.dataclass
 class MemoryRaster:
-    data: np.ndarray | np.ma.MaskedArray
+    data: np.ndarray | np.ma.MaskedArray | xr.DataArray
     transform: affine.Affine
     crs: str | None
     nodata: int | float
     origin_path: Path | None = None
     dtype: str = "float64"
+    driver: str = "GTiff"
 
     @property
     def shape(self):
@@ -75,6 +86,8 @@ class MemoryRaster:
             "crs": self.crs,
             "count": 1,
             "dtype": self.dtype,
+            "driver": self.driver,
+            "nodata": self.nodata,
         }
 
     def min(self) -> float:
@@ -95,11 +108,29 @@ class MemoryRaster:
         )
 
     @staticmethod
+    def from_xarray(
+        da: xr.DataArray,
+        c_longitude="longitude",
+        c_latitude="latitude",
+        nodata: int | float | None = None,
+    ) -> MemoryRaster:
+        "Creates MemoryRaster from xarray, assumes EPSG:4326"
+        transform = from_origin(
+            da[c_longitude].min(),
+            da[c_latitude].max(),
+            grid_size(da, c_longitude),
+           grid_size(da, c_latitude),
+        )
+        return MemoryRaster(
+            da, transform, "EPSG:4326", nodata=nodata or 0, dtype=da.dtype.name
+        )
+
+    @staticmethod
     def read(
         file: str | Path,
         crs: str | None = None,
         resampling: rasterio.enums.Resampling = rasterio.enums.Resampling.bilinear,
-    ):
+    ) -> MemoryRaster:
         "Reads from a file supported by rasterio"
 
         with rasterio.open(file) as src:
@@ -137,6 +168,15 @@ class MemoryRaster:
                 reprojected_data.squeeze(), transform, crs, src.nodata, origin_path
             )
 
+    @contextmanager
+    def as_rasterio(self):
+        with MemoryFile() as memfile:
+            with memfile.open(**self.profile) as dataset:
+                dataset.write(np.expand_dims(self.data, axis=0))
+
+            with memfile.open() as dataset:
+                yield dataset
+
     def mask(
         self, geometry: gpd.GeoDataFrame | gpd.GeoSeries, crop: bool = True
     ) -> MemoryRaster:
@@ -154,7 +194,7 @@ class MemoryRaster:
             # reassign geometry to GeoSeries
             geometry = geometry.geometry
         with MemoryFile() as memfile:
-            with memfile.open(driver="GTiff", **self.profile) as src:
+            with memfile.open(**self.profile) as src:
                 src.write(self.data, 1)
             with memfile.open() as src:
                 masked, transform = rasterio.mask.mask(src, geometry, crop=crop)
@@ -222,5 +262,32 @@ class MemoryRaster:
             np.ma.masked_equal(resampled_data, self.nodata), dst.transform, dst.crs, 0
         )
 
-    def zonal_stats(self, geometry: gpd.GeoDataFrame, **kwargs):
+    def zonal_stats(
+        self,
+        geometry: gpd.GeoDataFrame,
+        ops: str,
+        weights: MemoryRaster | None = None,
+        **kwargs,
+    ) -> pd.DataFrame | gpd.GeoDataFrame:
         "Calculate zonal statistics using exactextract"
+
+        with self.as_rasterio() as raster:
+            if weights:
+                with weights.as_rasterio() as weights_raster:
+                    out = exactextract.exact_extract(
+                        raster,
+                        geometry,
+                        ops,
+                        weights=weights_raster,
+                        output="pandas",
+                        **kwargs,
+                    )
+            else:
+                out = exactextract.exact_extract(
+                    raster, geometry, ops, output="pandas", **kwargs
+                )
+        if not isinstance(out, (pd.DataFrame, gpd.GeoDataFrame)):
+            raise ValueError(
+                f"exactextract failed to calculate zonal statistics, returned output:\n{out}"
+            )
+        return out
