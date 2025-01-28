2131a05327bf722994208f50ffb8c1f88f88893f
Abhishek Dasgupta <abhishek.dasgupta@dtc.ox.ac.uk>
2025-01-28 12:17:30 +0000

    Add geoglue.data_path

--- src/geoglue/__init__.py
+++ src/geoglue/__init__.py
@@ -1,2 +1,12 @@
-def hello() -> str:
-    return "Hello from geoglue!"
+from .memoryraster import MemoryRaster
+import os
+from pathlib import Path
+
+if data_home := os.getenv("XDG_DATA_HOME"):
+    data_path = Path(data_home) / "geoglue"
+else:
+    data_path = Path.home() / ".local" / "share" / "geoglue"
+if not data_path.exists():
+    data_path.mkdir(parents = True)
+
+__all__ = ["MemoryRaster"]
