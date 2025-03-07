636e3b3aa0eeff12940d94ced2f7381cf6abd641
Abhishek Dasgupta <abhishek.dasgupta@dtc.ox.ac.uk>
2025-03-07 16:32:07 +0000

    ci: use nox for testing (#22)

---
+++ noxfile.py
@@ -0,0 +1,28 @@
+import nox
+
+nox.options.default_venv_backend = "uv"
+
+
+@nox.session
+def lint(session):
+    session.install("ruff")
+    session.run("ruff", "check", "src")
+    session.run("ruff", "check", "tests")
+
+
+def fmt(session):
+    session.install("ruff")
+    session.run("ruff", "format", "--check")
+
+
+@nox.session
+def base(session):
+    session.env.update({"UV_PROJECT_ENVIRONMENT": session.virtualenv.location})
+    session.run("uv", "sync", "--all-extras", "--dev")
+    session.run("bash", "./scripts/install_cdo.sh", external=True)
+
+
+@nox.session
+def tests(session):
+    base(session)
+    session.run("uv", "run", "pytest", "--cov", "--cov-report=html")
