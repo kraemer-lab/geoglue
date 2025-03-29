afc23dca11eab6df5664b6c5a8ea4616d836f317
Abhishek Dasgupta <abhishek.dasgupta@dtc.ox.ac.uk>
2025-03-29 22:08:21 +0000

    scripts: refactor install to allow more dependencies later on

--- scripts/install_cdo.sh
+++ scripts/install_cdo.sh
@@ -1,28 +1,34 @@
 #!/bin/sh
-# Install CDO if possible
-#
 
-OS=$(uname -s)
-if ! command -v cdo; then
-    echo "CDO is not installed."
-    if [ "$OS" = "Darwin" ]; then
-        echo "Installing CDO on macOS..."
-        brew install cdo
-    elif [ "$OS" = "Linux" ]; then
-        # Check if Ubuntu or Debian-based system
-        if [ -f /etc/os-release ]; then
-            # shellcheck disable=SC1091
-            . /etc/os-release
-            if [ "$ID" = "ubuntu" ] || [ "$ID_LIKE" = "debian" ]; then
-                echo "Installing CDO on Ubuntu/Debian..."
-                sudo apt-get update && sudo apt-get install --no-install-recommends -y cdo
-            else
-                echo "Unsupported Linux distribution: $ID"
+OS="$(uname -s)"
+
+install_if_absent() {
+    cmd="$1"
+    pkgname_macos="${2:-1}"
+    pkgname_debian="${3:-1}"
+    if ! command -v "$cmd" > /dev/null; then
+        echo "$1 is not installed."
+        if [ "$OS" = "Darwin" ]; then
+            echo "Installing $1 on macOS..."
+            brew install "$pkgname_macos"
+        elif [ "$OS" = "Linux" ]; then
+            # Check if Ubuntu or Debian-based system
+            if [ -f /etc/os-release ]; then
+                # shellcheck disable=SC1091
+                . /etc/os-release
+                if [ "$ID" = "ubuntu" ] || [ "$ID_LIKE" = "debian" ]; then
+                    echo "Installing CDO on Ubuntu/Debian..."
+                    sudo apt-get update && sudo apt-get install --no-install-recommends -y "$pkgname_debian"
+                else
+                    echo "Unsupported Linux distribution: $ID"
+                fi
             fi
+        else
+            echo "Unsupported OS: $OS"
         fi
     else
-        echo "Unsupported OS: $OS"
+        echo "installed $(command -v "$cmd")"
     fi
-else
-	echo "CDO is already installed."
-fi
+}
+
+install_if_absent cdo
