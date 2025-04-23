48ad6a1bebdcd43749926758f9e3c6b26ce79cd8
Abhishek Dasgupta <abhishek.dasgupta@dtc.ox.ac.uk>
2025-04-23 23:33:42 +0100

    feat(resample): add resampled_dataset() context manager
    Fixes: #38

--- src/geoglue/resample.py
+++ src/geoglue/resample.py
@@ -1,8 +1,12 @@
+import os
 import tempfile
 from pathlib import Path
 from typing import Literal
+from collections.abc import Iterator
+from contextlib import contextmanager
 
 import cdo
+import xarray as xr
 
 from .memoryraster import MemoryRaster
 from .util import is_lonlat
@@ -15,7 +19,27 @@ def resample(
     outfile: str | Path | None = None,
     skip_exists=True,
 ) -> Path:
-    "Resamples input file to output file using CDO's resampling to a target raster grid"
+    """Resamples input file to output file using CDO's resampling to a target raster grid
+
+    Parameters
+    ----------
+    resampling
+        Resampling type to use, must be one of `remapbil` or `remapdis`
+    infile
+        Input file to read
+    target
+        Target MemoryRaster whose grid to resample to
+    outfile
+        Output resampled file path, if not specified, generated from infile by
+        affixing `.resampled` to the path
+    skip_exists
+        Whether to skip resampling if outfile exists (default=True)
+
+    Returns
+    -------
+    Resampled dataset path
+    """
+
     _cdo = cdo.Cdo()
     if isinstance(infile, str):
         infile = Path(infile)
@@ -40,3 +64,50 @@ def resample(
             case "remapdis":
                 _cdo.remapdis(griddes.name, input=str(infile), output=str(outfile))
         return Path(outfile)
+
+
+@contextmanager
+def resampled_dataset(
+    resampling: Literal["remapbil", "remapdis"],
+    data: str | Path | xr.Dataset,
+    target: MemoryRaster,
+) -> Iterator[xr.Dataset]:
+    """Context manager version of :meth:`geoglue.resample.resample`.
+
+    Parameters
+    ----------
+    resampling
+        Resampling type to use, must be one of `remapbil` or `remapdis`
+    data
+        Input file to read or xarray dataset
+    target
+        Target MemoryRaster whose grid to resample to
+
+    Yields
+    ------
+    Resampled dataset
+
+    Example
+    -------
+    >>> from geoglue.resample import resampled_dataset
+    >>> from geoglue.country import Country
+    >>> country = Country('ABC')
+    >>> with resampled_dataset("remapbil", "somefile.nc", country.population_raster(2020)) as ds:
+    ...     print(ds)
+    """
+    if isinstance(data, xr.Dataset):
+        # write to temporary file
+        fd, data_path = tempfile.mkstemp(prefix="geoglue-", suffix=".nc")
+        data.to_netcdf(data_path)  # type: ignore
+        infile_istempfile = data_path, True
+    else:
+        infile_istempfile = Path(data), False
+
+    with tempfile.NamedTemporaryFile(prefix="geoglue-", suffix=".nc") as f:
+        resample(resampling, infile_istempfile[0], target, f.name, skip_exists=False)
+        ds = xr.open_dataset(f.name, engine="netcdf4")
+        yield ds
+
+    if infile_istempfile[1]:  # clean up temporary file
+        os.close(fd)  # type: ignore
+        os.unlink(infile_istempfile[0])
