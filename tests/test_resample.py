fef16baa15396c295c165d5d249cddb944d2a11c
Abhishek Dasgupta <abhishek.dasgupta@dtc.ox.ac.uk>
2025-04-23 23:33:42 +0100

    feat(resample): add resampled_dataset() context manager
    Fixes: #38

--- tests/test_resample.py
+++ tests/test_resample.py
@@ -1,15 +1,23 @@
 import xarray as xr
 
-from geoglue.resample import resample
+from geoglue.resample import resample, resampled_dataset
 from geoglue.memoryraster import MemoryRaster
 
 import pytest
 
+INFILE = "data/VNM/era5/VNM-2020-era5.daily_sum.nc"
+
 
 @pytest.mark.parametrize("resampling", ["remapbil", "remapdis"])
 def test_resample(resampling, population_1km):
-    infile = "data/VNM/era5/VNM-2020-era5.daily_sum.nc"
-    outfile = resample(resampling, infile, target=population_1km, skip_exists=False)
+    outfile = resample(resampling, INFILE, target=population_1km, skip_exists=False)
     outrast = MemoryRaster.from_xarray(xr.open_dataset(outfile).tp.isel(valid_time=0))
     assert outrast.griddes.approx_equal(population_1km.griddes)
     outfile.unlink()
+
+
+@pytest.mark.parametrize("resampling", ["remapbil", "remapdis"])
+def test_resampled_dataset(resampling, population_1km):
+    with resampled_dataset(resampling, INFILE, target=population_1km) as ds:
+        outrast = MemoryRaster.from_xarray(ds.tp.isel(valid_time=0))
+        assert outrast.griddes.approx_equal(population_1km.griddes)
