82433114113b101002a14fbb2913cee31f1c6490
Abhishek Dasgupta <abhishek.dasgupta@dtc.ox.ac.uk>
2025-09-28 22:52:45 +0100

    Change Region type to include all admin levels at once

--- tests/test_zonal_stats.py
+++ tests/test_zonal_stats.py
@@ -15,7 +15,7 @@ ADMIN2_N = 706
 
 @pytest.fixture(scope="module")
 def vnm_geoboundaries_admin2():
-    df = geoboundaries("VNM", 2, data_path=DATA_PATH).read()
+    df = geoboundaries("VNM", data_path=DATA_PATH).read(2)
 
     # We use era5_extents which use admin0 boundaries. In geoBoundaries data,
     # the admin0 total_bounds are smaller than the admin2 total bounds which
