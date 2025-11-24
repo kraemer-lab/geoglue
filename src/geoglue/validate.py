7d6df51b592a0ca1a3090b30a3f68731cdafaedc
Abhishek Dasgupta <abhishek.dasgupta@dtc.ox.ac.uk>
2025-11-24 12:59:24 +0000

    cli: rename validate -> stats, support multiple files and xr.Dataset

--- src/geoglue/validate.py
+++ src/geoglue/validate.py
@@ -1,27 +1,73 @@
 import itertools
+from pathlib import Path
+from typing import NamedTuple
 
 import xarray as xr
 
+CoordinateValue = dict[str, int]
 
-def stats_nonregion(da: xr.DataArray):
+
+class VariableStatistics(NamedTuple):
+    mean: float
+    min: float
+    max: float
+
+
+class CoordStatistics(NamedTuple):
+    coord: CoordinateValue
+    stat: VariableStatistics
+
+    def __str__(self) -> str:
+        _coord_str = " ".join(f"{k}={v}" for k, v in self.coord.items())
+        return f"{_coord_str}\tmean={self.stat.mean:.5f}\tmin={self.stat.min:.5f}\tmax={self.stat.max:.5f}"
+
+
+class DataArrayStatistics(NamedTuple):
+    name: str
+    nna: int  # number of NA values
+    coord_stats: list[CoordStatistics]
+
+
+def stats(
+    da: xr.DataArray,
+) -> DataArrayStatistics:
     """
     Prints statistics for each combination of non-region indices
     """
-    isnull = da.isnull().sum().item()
-    if isnull:
-        print(f"[!!] {isnull} NaN values detected")
-    else:
-        print("[OK] No NaN values detected")
+    if "region" not in da.dims:
+        raise ValueError(
+            "Not a zonalstats file (.zs.nc), must have the 'region' coordinate"
+        )
+    nna = da.isnull().sum().item()
     non_region_dims = [dim for dim in da.dims if dim != "region"]
     dim_index_ranges = {dim: range(da.sizes[dim]) for dim in non_region_dims}
 
     # Iterate over all combinations
+    out = []
     for combo in itertools.product(*dim_index_ranges.values()):
-        indexer = dict(zip(non_region_dims, combo))
-        indexer_s = " ".join(f"{k}={v}" for k, v in indexer.items())
+        indexer = dict(zip(map(str, non_region_dims), combo))
 
         subset = da.isel(**indexer)  # type: ignore
         mean = subset.mean(dim="region").item()
         minimum = subset.min(dim="region").item()
         maximum = subset.max(dim="region").item()
-        print(f"{indexer_s}\tmean={mean:.4f}\tmin={minimum:.4f}\tmax={maximum:.4f}")
+        out.append(
+            CoordStatistics(
+                indexer, VariableStatistics(mean=mean, min=minimum, max=maximum)
+            )
+        )
+    name = str(da.name) if da.name else "var"
+    return DataArrayStatistics(name, nna, out)
+
+
+def print_file_stats(file: Path):
+    ds = xr.open_dataset(file)
+    for var in sorted(ds.data_vars):
+        st = stats(ds[var])
+        nna = st.nna
+        if nna == 0:
+            print(f"\033[1m{file.name}:{var}: nna={nna}\033[0m")
+        else:
+            print(f"\033[1m{file.name}:{var}: nna=\033[1;91{nna}\033[0m")
+        for coord_stat in st.coord_stats:
+            print(f"{file.name}:{var}: {coord_stat}")
