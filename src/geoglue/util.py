df01c9a024df60abc899a8ac80e689b3e1f66bde
Quynh Anh <90673616+quingzz@users.noreply.github.com>
2026-09-03 12:01:27 +0700

    remove redundant func

--- src/geoglue/util.py
+++ src/geoglue/util.py
@@ -119,16 +119,16 @@ def get_last_sunday(x: int | datetime.date) -> datetime.date:
 
         return d
     
-def get_last_sunday(date: datetime.date) -> datetime.date:
-    "Gets the previous Sunday given a specific date"
-    days_since_sunday = (date.weekday() + 1) % 7
-    return date - datetime.timedelta(days=days_since_sunday)
+# def get_last_sunday(date: datetime.date) -> datetime.date:
+#     "Gets the previous Sunday given a specific date"
+#     days_since_sunday = (date.weekday() + 1) % 7
+#     return date - datetime.timedelta(days=days_since_sunday)
 
-    if isinstance(x, datetime.date):
-        days_since_sunday = (x.weekday() + 1) % 7
-        return x - datetime.timedelta(days=days_since_sunday)
+#     if isinstance(x, datetime.date):
+#         days_since_sunday = (x.weekday() + 1) % 7
+#         return x - datetime.timedelta(days=days_since_sunday)
 
-    raise TypeError(f"Expected int or datetime.date, got {type(x).__name__}")
+#     raise TypeError(f"Expected int or datetime.date, got {type(x).__name__}")
 
 
 def sha256(file_path: str | Path, prefix: bool = False) -> str:
